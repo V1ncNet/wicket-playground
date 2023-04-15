debbbd18453813f42c69ba5b58d7414ba1218282
Vincent Nadoll <vincent.nadoll@googlemail.com>
2023-04-16 01:03:30 +0200

    Extract resource adapter

--- src/main/java/de/vinado/app/playground/wicket/PlaygroundCssResourceReference.java
+++ src/main/java/de/vinado/app/playground/wicket/PlaygroundCssResourceReference.java
@@ -1,21 +1,13 @@
 package de.vinado.app.playground.wicket;
 
-import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
+import de.vinado.app.playground.wicket.resource.CssResourceStreamAdapter;
 import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.CssReferenceHeaderItem;
 import org.apache.wicket.request.resource.IResource;
 import org.apache.wicket.request.resource.ResourceReference;
 import org.apache.wicket.request.resource.ResourceStreamResource;
-import org.apache.wicket.util.lang.Bytes;
-import org.apache.wicket.util.resource.AbstractResourceStream;
-import org.apache.wicket.util.resource.IFixedLocationResourceStream;
-import org.apache.wicket.util.resource.ResourceStreamNotFoundException;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 public class PlaygroundCssResourceReference extends ResourceReference {
 
@@ -35,48 +27,6 @@ public class PlaygroundCssResourceReference extends ResourceReference {
         return new ResourceStreamResource(new CssResourceStreamAdapter(resource));
     }
 
-
-    @RequiredArgsConstructor
-    private static class CssResourceStreamAdapter extends AbstractResourceStream
-        implements IFixedLocationResourceStream {
-
-        private final Resource resource;
-
-        private InputStream stream;
-
-        @Override
-        public InputStream getInputStream() throws ResourceStreamNotFoundException {
-            try {
-                return stream = resource.getInputStream();
-            } catch (IOException e) {
-                throw new ResourceStreamNotFoundException(e);
-            }
-        }
-
-        @Override
-        public void close() throws IOException {
-            if (null != stream) {
-                stream.close();
-            }
-        }
-
-        @Override
-        @SneakyThrows
-        public String locationAsString() {
-            return resource.getURI().toString();
-        }
-
-        @Override
-        @SneakyThrows
-        public Bytes length() {
-            return Bytes.bytes(resource.contentLength());
-        }
-
-        @Override
-        public String getContentType() {
-            return "text/css";
-        }
-    }
 
     private static class Holder {
 
---
+++ src/main/java/de/vinado/app/playground/wicket/resource/CssResourceStreamAdapter.java
@@ -0,0 +1,54 @@
+package de.vinado.app.playground.wicket.resource;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.apache.wicket.util.lang.Bytes;
+import org.apache.wicket.util.resource.AbstractResourceStream;
+import org.apache.wicket.util.resource.IFixedLocationResourceStream;
+import org.apache.wicket.util.resource.ResourceStreamNotFoundException;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@RequiredArgsConstructor
+public class CssResourceStreamAdapter extends AbstractResourceStream
+    implements IFixedLocationResourceStream {
+
+    private final Resource resource;
+
+    private InputStream stream;
+
+    @Override
+    public InputStream getInputStream() throws ResourceStreamNotFoundException {
+        try {
+            return stream = resource.getInputStream();
+        } catch (IOException e) {
+            throw new ResourceStreamNotFoundException(e);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (null != stream) {
+            stream.close();
+        }
+    }
+
+    @Override
+    @SneakyThrows
+    public String locationAsString() {
+        return resource.getURI().toString();
+    }
+
+    @Override
+    @SneakyThrows
+    public Bytes length() {
+        return Bytes.bytes(resource.contentLength());
+    }
+
+    @Override
+    public String getContentType() {
+        return "text/css";
+    }
+}
