8b68bd6238034147e6c24d1d1264140096999fb3
Vincent Nadoll <vincent.nadoll@googlemail.com>
2023-04-15 14:28:20 +0200

    Extract base page

--- src/main/java/de/vinado/app/playground/wicket/HomePage.java
+++ src/main/java/de/vinado/app/playground/wicket/HomePage.java
@@ -1,21 +1,4 @@
 package de.vinado.app.playground.wicket;
 
-import org.apache.wicket.Component;
-import org.apache.wicket.markup.head.filter.HeaderResponseContainer;
-import org.apache.wicket.markup.html.WebPage;
-
-import static de.vinado.app.playground.wicket.DefaultJavaScriptFilteredIntoFooterHeaderResponseDecorator.DEFAULT_FOOTER_FILTER_NAME;
-
-public class HomePage extends WebPage {
-
-    @Override
-    protected void onInitialize() {
-        super.onInitialize();
-
-        add(footerBucket("footer-bucket"));
-    }
-
-    private Component footerBucket(String wicketId) {
-        return new HeaderResponseContainer(wicketId, DEFAULT_FOOTER_FILTER_NAME);
-    }
+public class HomePage extends PlaygroundPage {
 }
