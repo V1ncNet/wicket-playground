f8b091d7375ac707b3e0cd21e71cbf1b58e7b093
Vincent Nadoll <vincent.nadoll@googlemail.com>
2023-04-15 11:41:49 +0200

    Enable @SpringBean-annotations

--- src/main/java/de/vinado/app/playground/wicket/WicketApplication.java
+++ src/main/java/de/vinado/app/playground/wicket/WicketApplication.java
@@ -1,12 +1,20 @@
 package de.vinado.app.playground.wicket;
 
+import lombok.Setter;
 import org.apache.wicket.Page;
+import org.apache.wicket.application.ComponentInstantiationListenerCollection;
 import org.apache.wicket.mock.MockHomePage;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class WicketApplication extends WebApplication {
+public class WicketApplication extends WebApplication implements ApplicationContextAware {
+
+    @Setter
+    private ApplicationContext applicationContext;
 
     @Override
     public Class<? extends Page> getHomePage() {
@@ -17,7 +25,14 @@ public class WicketApplication extends WebApplication {
     protected void init() {
         super.init();
 
+        ComponentInstantiationListenerCollection componentInstantiationListeners = getComponentInstantiationListeners();
+        configure(componentInstantiationListeners);
+
         mountPages();
+    }
+
+    private void configure(ComponentInstantiationListenerCollection listeners) {
+        listeners.add(new SpringComponentInjector(this, applicationContext));
     }
 
     private void mountPages() {
