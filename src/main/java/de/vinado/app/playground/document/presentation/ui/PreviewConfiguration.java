a278e022b8e1599bc7ed713b4e7b23599e1091e2
Vincent Nadoll <vincent.nadoll@googlemail.com>
2023-04-17 18:18:56 +0200

    Proxy dummy PDF through preview service

--- src/main/java/de/vinado/app/playground/document/presentation/ui/PreviewConfiguration.java
+++ src/main/java/de/vinado/app/playground/document/presentation/ui/PreviewConfiguration.java
@@ -1,16 +1,23 @@
 package de.vinado.app.playground.document.presentation.ui;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.wicket.csp.CSPDirective;
 import org.apache.wicket.csp.ContentSecurityPolicySettings;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Profile("wicket")
 @Configuration
+@EnableConfigurationProperties(PreviewProperties.class)
+@RequiredArgsConstructor
 public class PreviewConfiguration {
+
+    private final PreviewProperties properties;
 
     @Autowired
     public void init(WebApplication webApplication) {
@@ -20,11 +27,15 @@ public class PreviewConfiguration {
 
     private void configure(ContentSecurityPolicySettings settings) {
         settings.blocking()
-            .add(CSPDirective.FRAME_SRC, "*");
+            .add(CSPDirective.FRAME_SRC, properties.getBaseUrl() + "/");
     }
 
     @Bean
     public PreviewUrlProvider previewUrlProvider() {
-        return document -> document.getUri().toURL();
+        return document -> UriComponentsBuilder.fromHttpUrl(properties.getBaseUrl().toString())
+            .path("/preview/pdf")
+            .query("url={url}")
+            .build(document.getUri())
+            .toURL();
     }
 }
