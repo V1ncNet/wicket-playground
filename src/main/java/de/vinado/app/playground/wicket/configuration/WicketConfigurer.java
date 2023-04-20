9a4bce633eff19ab3a621c4d081d22fc19c09e9f
Vincent Nadoll <vincent.nadoll@googlemail.com>
2023-04-20 16:48:54 +0200

    Provide navigation item registration callback

--- src/main/java/de/vinado/app/playground/wicket/configuration/WicketConfigurer.java
+++ src/main/java/de/vinado/app/playground/wicket/configuration/WicketConfigurer.java
@@ -1,9 +1,13 @@
 package de.vinado.app.playground.wicket.configuration;
 
+import de.vinado.app.playground.wicket.navigation.NavigationItemRegistry;
 import org.apache.wicket.protocol.http.WebApplication;
 
 public interface WicketConfigurer {
 
     default void init(WebApplication webApplication) {
+    }
+
+    default void addNavigationItems(NavigationItemRegistry registry) {
     }
 }
---
+++ src/main/java/de/vinado/app/playground/wicket/navigation/NavigationItemRegistry.java
@@ -0,0 +1,7 @@
+package de.vinado.app.playground.wicket.navigation;
+
+@FunctionalInterface
+public interface NavigationItemRegistry {
+
+    void register(NavigationItem navigationItem);
+}
