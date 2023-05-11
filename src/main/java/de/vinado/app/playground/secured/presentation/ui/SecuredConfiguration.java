dee2f7dd32647544cdb135c974d76ae7d3caf80a
Vincent Nadoll <vincent.nadoll@googlemail.com>
2023-05-11 15:20:41 +0200

    Add secured Wicket page

---
+++ src/main/java/de/vinado/app/playground/secured/presentation/ui/SecuredConfiguration.java
@@ -0,0 +1,39 @@
+package de.vinado.app.playground.secured.presentation.ui;
+
+import de.vinado.app.playground.wicket.configuration.WicketConfigurer;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Order(Ordered.HIGHEST_PRECEDENCE + 100)
+@Profile("wicket")
+@Configuration
+public class SecuredConfiguration implements WicketConfigurer {
+
+    private static final String PATH = "secured";
+
+    @Bean
+    public SecurityFilterChain securedFilterChain(HttpSecurity http) throws Exception {
+        http.antMatcher("/" + PATH + "/**")
+            .authorizeHttpRequests(authorize -> authorize
+                .anyRequest().authenticated())
+            .oauth2Login()
+        ;
+
+        return http.build();
+    }
+
+    @Override
+    public void init(WebApplication webApplication) {
+        mountPages(webApplication);
+    }
+
+    private void mountPages(WebApplication webApplication) {
+        webApplication.mountPage(PATH, SecuredPage.class);
+    }
+}
