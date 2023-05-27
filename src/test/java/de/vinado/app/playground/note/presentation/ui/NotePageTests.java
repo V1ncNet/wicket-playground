742efb280eb4f0377bd4bb5b153aec7cd951bafa
Vincent Nadoll <vincent.nadoll@googlemail.com>
2023-05-27 13:46:50 +0200

    Add component smoke tests

---
+++ src/test/java/de/vinado/app/playground/note/presentation/ui/NotePageTests.java
@@ -0,0 +1,62 @@
+package de.vinado.app.playground.note.presentation.ui;
+
+import de.vinado.app.playground.wicket.codimd.CodiMd;
+import de.vinado.app.playground.wicket.codimd.CodiMdUrlProvider;
+import de.vinado.app.playground.wicket.navigation.NavigationItemSupplier;
+import de.vinado.app.playground.wicket.test.SpringEnabledWicketTestCase;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.net.URL;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@ContextConfiguration(classes = NoteConfiguration.class)
+class NotePageTests extends SpringEnabledWicketTestCase {
+
+    @MockBean
+    private NavigationItemSupplier supplier;
+
+    @MockBean
+    private CodiMdUrlProvider urlProvider;
+
+    @BeforeEach
+    @SneakyThrows
+    void setUp() {
+        when(supplier.get()).thenAnswer(i -> Stream.empty());
+        when(urlProvider.resolve(any())).thenReturn(randomUrl());
+    }
+
+    @SneakyThrows
+    private static URL randomUrl() {
+        UUID host = UUID.randomUUID();
+        return new URL("http://" + host);
+    }
+
+    @Test
+    void callingUrl_shouldRenderPage() {
+        tester.executeUrl("note");
+
+        tester.assertRenderedPage(NotePage.class);
+    }
+
+    @Test
+    void renderingPage_shouldRenderPage() {
+        tester.startPage(NotePage.class);
+
+        tester.assertRenderedPage(NotePage.class);
+    }
+
+    @Test
+    void renderingPage_shouldRenderCodiMdPad() {
+        tester.startPage(NotePage.class);
+
+        tester.assertComponent("navigation:navigation_body:codiMd", CodiMd.class);
+    }
+}
---
+++ src/test/java/de/vinado/app/playground/wicket/codimd/CodiMdTests.java
@@ -0,0 +1,48 @@
+package de.vinado.app.playground.wicket.codimd;
+
+import de.vinado.app.playground.wicket.test.SpringEnabledWicketTestCase;
+import lombok.SneakyThrows;
+import org.apache.wicket.model.Model;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.net.URL;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+class CodiMdTests extends SpringEnabledWicketTestCase {
+
+    private static final String WICKET_ID = "pad";
+
+    @MockBean
+    private CodiMdUrlProvider urlProvider;
+
+    @BeforeEach
+    @SneakyThrows
+    void setUp() {
+        when(urlProvider.resolve(any())).thenReturn(randomUrl());
+    }
+
+    @SneakyThrows
+    private static URL randomUrl() {
+        UUID host = UUID.randomUUID();
+        return new URL("http://" + host);
+    }
+
+    @Test
+    void startingWithRandomNoteId_shouldRenderComponent() {
+        CodiMd panel = new CodiMd(WICKET_ID, this::randomNote);
+
+        tester.startComponentInPage(panel);
+
+        tester.assertComponent(WICKET_ID, CodiMd.class);
+    }
+
+    private Note randomNote() {
+        UUID id = UUID.randomUUID();
+        return new Note(id);
+    }
+}
