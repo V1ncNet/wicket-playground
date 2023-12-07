624af605f6a5e5b0e01c0f0076659822e93bcdf5
Vincent Nadoll <vincent.nadoll@googlemail.com>
2023-12-07 12:14:36 +0100

    Disable Spring Security CSRF protection

--- src/main/java/de/vinado/app/playground/wicket/WicketConfiguration.java
+++ src/main/java/de/vinado/app/playground/wicket/WicketConfiguration.java
@@ -9,6 +9,11 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.EnumSet;
 
@@ -41,5 +46,15 @@ public class WicketConfiguration {
         registration.setAsyncSupported(true);
         registration.addUrlPatterns(APP_ROOT);
         return registration;
+    }
+
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    @Bean
+    public SecurityFilterChain wicketFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+        ;
+
+        return http.build();
     }
 }
