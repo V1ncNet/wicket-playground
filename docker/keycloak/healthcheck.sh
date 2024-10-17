0e9b9ef91af21359c5ca22df106d87d6f1a87acd
Vincent Nadoll <vincent.nadoll@googlemail.com>
2024-10-17 12:04:53 +0200

    Route everything through reverse proxy

--- docker/keycloak/healthcheck.sh
+++ docker/keycloak/healthcheck.sh
@@ -2,7 +2,7 @@
 
 exec 3<>/dev/tcp/localhost/${KC_HTTP_MANAGEMENT_PORT:-9000}
 
-echo -e "GET /health/ready HTTP/1.1\nhost: localhost:${KC_HTTP_MANAGEMENT_PORT:-9000}\n" >&3
+echo -e "GET ${KC_HTTP_RELATIVE_PATH}/health/ready HTTP/1.1\nhost: localhost:${KC_HTTP_MANAGEMENT_PORT:-9000}\n" >&3
 
 timeout --preserve-status 1 cat <&3 | grep -m 1 status | grep -m 1 UP
 error=$?
