571ca4bfa8553fea3e3f387f5556bff23729dd7b
Vincent Nadoll <vincent.nadoll@googlemail.com>
2024-10-19 21:49:52 +0200

    Configure silent token refresh

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
 
 type Props = {
   children: Readonly<ReactNode>;
@@ -8,7 +9,9 @@ export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
     <body>
+    <SessionProvider refetchInterval={300}>
     {children}
+    </SessionProvider>
     </body>
     </html>
   );
