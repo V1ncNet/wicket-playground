e3470fac8a924aced1e2803122bd0decc63acca4
Vincent Nadoll <vincent.nadoll@googlemail.com>
2025-03-25 11:23:39 +0100

    Stub modal component

--- src/main/java/de/vinado/app/playground/bootstrap/presentation/ui/BootstrapPage.java
+++ src/main/java/de/vinado/app/playground/bootstrap/presentation/ui/BootstrapPage.java
@@ -1,6 +1,24 @@
 package de.vinado.app.playground.bootstrap.presentation.ui;
 
 import de.vinado.app.playground.wicket.PlaygroundPage;
+import de.vinado.app.playground.wicket.bootstrap.modal.Modal;
 
 public class BootstrapPage extends PlaygroundPage {
+
+    private final Modal modal;
+
+    public BootstrapPage() {
+        this.modal = modal("modal");
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+
+        queue(modal);
+    }
+
+    private Modal modal(String wicketId) {
+        return new Modal(wicketId);
+    }
 }
---
+++ src/main/java/de/vinado/app/playground/wicket/bootstrap/modal/Modal.java
@@ -0,0 +1,10 @@
+package de.vinado.app.playground.wicket.bootstrap.modal;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class Modal extends Panel {
+
+    public Modal(String id) {
+        super(id);
+    }
+}
