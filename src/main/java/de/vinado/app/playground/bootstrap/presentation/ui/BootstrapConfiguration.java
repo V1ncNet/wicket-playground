d08004ec2db21d9e12ef8ec64bd96b53b09b63b3
Vincent Nadoll <vincent.nadoll@googlemail.com>
2025-04-02 17:53:07 +0200

    Restore previous page order

--- src/main/java/de/vinado/app/playground/bootstrap/presentation/ui/BootstrapConfiguration.java
+++ src/main/java/de/vinado/app/playground/bootstrap/presentation/ui/BootstrapConfiguration.java
@@ -4,14 +4,15 @@ import de.vinado.app.playground.wicket.bootstrap.icon.Bi;
 import de.vinado.app.playground.wicket.configuration.WicketConfigurer;
 import de.vinado.app.playground.wicket.navigation.NavigationItem;
 import de.vinado.app.playground.wicket.navigation.NavigationItemRegistry;
-import lombok.RequiredArgsConstructor;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 
-@Order(Ordered.HIGHEST_PRECEDENCE)
+import lombok.RequiredArgsConstructor;
+
+@Order(Ordered.HIGHEST_PRECEDENCE + 50)
 @Profile("wicket")
 @Configuration
 @RequiredArgsConstructor
