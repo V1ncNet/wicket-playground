fc2b2536d7f1bce0ac9ef0437d170a292bf43b29
Vincent Nadoll <vincent.nadoll@googlemail.com>
2025-04-18 23:14:32 +0200

    Fix Keycloak health check script

--- docker/keycloak/healthcheck.sh
+++ docker/keycloak/healthcheck.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-exec 3<>/dev/tcp/localhost/${KC_HTTP_PORT:-8080}
+exec 3<>/dev/tcp/localhost/${KC_HTTP_MANAGEMENT_PORT:-9000}
 
-echo -e "GET /health/ready HTTP/1.1\nhost: localhost:${KC_HTTP_PORT:-8080}\n" >&3
+echo -e "GET ${KC_HTTP_MANAGEMENT_RELATIVE_PATH}/health/ready HTTP/1.1\nhost: localhost:${KC_HTTP_MANAGEMENT_PORT:-9000}\n" >&3
 
 timeout --preserve-status 1 cat <&3 | grep -m 1 status | grep -m 1 UP
 error=$?
