3aeed483f29cc6d23af7f4818bc0b490beb94d10
Vincent Nadoll <vincent.nadoll@googlemail.com>
2025-04-19 01:08:42 +0200

    Move modal dialog form component

--- src/main/java/de/vinado/app/playground/bootstrap/presentation/ui/BootstrapComponentsPage.java
+++ src/main/java/de/vinado/app/playground/bootstrap/presentation/ui/BootstrapComponentsPage.java
@@ -1,5 +1,7 @@
 package de.vinado.app.playground.bootstrap.presentation.ui;
 
+import de.vinado.app.playground.bootstrap.presentation.ui.modal.DemoDto;
+import de.vinado.app.playground.bootstrap.presentation.ui.modal.DemoModalDialogForm;
 import de.vinado.app.playground.wicket.PlaygroundPage;
 import de.vinado.app.playground.wicket.bootstrap.BootstrapPage;
 import org.apache.wicket.ajax.AjaxRequestTarget;
@@ -14,27 +16,26 @@ import static de.vinado.app.playground.wicket.bootstrap.modal.Modal.SubmitAction
 
 public class BootstrapComponentsPage extends PlaygroundPage {
 
-    private final IModel<ModalForm.Bean> model;
-
-    public BootstrapComponentsPage() {
-        this.model = new CompoundPropertyModel<>(new ModalForm.Bean());
-    }
-
     @Override
     protected void onInitialize() {
         super.onInitialize();
 
-        queue(new AjaxLink<>("openButton") {
+        queue(demoFormModalDialogButton("demoFormModalDialogButton"));
+    }
+
+    private AjaxLink<Object> demoFormModalDialogButton(String wicketId) {
+        IModel<DemoDto> model = new CompoundPropertyModel<>(new DemoDto());
+        return new AjaxLink<>(wicketId) {
 
             @Override
             public void onClick(AjaxRequestTarget target) {
                 BootstrapPage.get().modal()
-                    .title(Model.of("Bootstrap Modal"))
-                    .content(id -> new ModalForm(id, model))
+                    .title(Model.of("Bootstrap Modal Dialog Demo"))
+                    .content(id -> new DemoModalDialogForm(id, model))
                     .addAction(id -> new CloseAction(id, new ResourceModel("cancel", "Cancel")))
                     .addAction(id -> new SubmitAction(id, new ResourceModel("submit", "Submit")))
                     .show(target);
             }
-        });
+        };
     }
 }
