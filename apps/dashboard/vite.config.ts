b007ea645bc582867bf12edc63651a11e56e7e3b
Vincent Nadoll <vincent.nadoll@googlemail.com>
2025-08-24 11:41:55 +0200

    Inject runtime configuration in vite dev server
    The has a caveat though. Wicket resource references are initialized and
    available once the dispatcher Servlet is initialized which happens
    lazily. You need to visit any Wicket page first in order to initialize
    the Servlet.

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -1,23 +1,35 @@
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv, type UserConfig } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    tanstackRouter({
-      target: "react",
-      autoCodeSplitting: false,
-    }),
-    react(),
-  ],
-  build: {
-    rollupOptions: {
-      input: "./src/main.tsx",
-      output: {
-        entryFileNames: "dashboard.js",
-        assetFileNames: "[name].[ext]",
+export default ({ mode }: { mode: string }): UserConfig => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [
+      tanstackRouter({
+        target: "react",
+        autoCodeSplitting: false,
+      }),
+      react(),
+    ],
+    build: {
+      rollupOptions: {
+        input: "./src/main.tsx",
+        output: {
+          entryFileNames: "dashboard.js",
+          assetFileNames: "[name].[ext]",
+        },
       },
     },
-  },
-});
+    server: {
+      proxy: {
+        "/dashboard-config.js": {
+          target: process.env.VITE_SERVER_CONFIG_BASE_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  });
+}
